fb549fc4b4537a782f88c76f827acbb823e50887
Irirwanirira <irijoseph41@gmail.com>
2023-01-04 00:32:02 +0200

    Fix linters

--- nameable.rb
+++ nameable.rb
@@ -1,22 +1,22 @@
-# require_relative 'person'
+require_relative 'person'
 
 class Nameable
   def correct_name
-    raise NotImplementedError, "not implemented"
+    raise NotImplementedError, 'not implemented'
   end
 end
 
 class BaseDecorator < Nameable
-    atrr_accessor :nameable
+  atrr_accessor :nameable
 
-    def initilize(nameable)
-      super()
-      @nameable = nameable
-    end
+  def initilize(nameable)
+    super()
+    @nameable = nameable
+  end
 
-    def correct_name
-      @nameable.correct_name
-    end
+  def correct_name
+    @nameable.correct_name
+  end
 end
 
 class CapitilizeDecorator < BaseDecorator
@@ -27,17 +27,13 @@ end
 
 class TrimmerDecorator < BaseDecorator
   def correct_name
-    if @nameable.correct_name > 10
-      @nameable.correct_name.delete(@nameable.correct_name[10..])
-    else
-      @nameable.correct_name
-    end
+    @nameable.correct_name[0, 10]
   end
 end
 
 person = Person.new(22, 'maximilianus')
 person.correct_name
-capitalizedPerson = CapitalizeDecorator.new(person)
-capitalizedPerson.correct_name
-capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-capitalizedTrimmedPerson.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+capitalized_trimmed_person.correct_name
